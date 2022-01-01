f6b18e0d1d7aa1c6fbf35bda7d742e10b6fda468
Muhammad Shah Sawar <50781826+shahsawar92@users.noreply.github.com>
2022-01-01 15:08:47 +0500

    latest resolved bug

--- src/Components/Home/Components/header-2.js
+++ src/Components/Home/Components/header-2.js
@@ -9,9 +9,9 @@ export default function HeaderTwo({setshowModal}) {
 
         <div className='flex justify-evenly flex-wrap bg-white mx-2 my-2 h-auto  lg:mx-2 lg:my-2 lg:h-20 2xl:mx-3 rounded-lg 2xl:my-3 2xl:h-32 items-center'>
 {/* Candidate varification */}
-            <div  id="btn-1" className='pointer text-white lg:text-sm font-medium 2xl:py-2 lg:py-2 lg:w-48 lg:px-2 w-40 py-2 px-1 text-xs 2xl:w-60 mt-2  rounded-lg  2xl:px-2 flex justify-between'> <div onClick={()=>{setshowModal(true) }}> Candidate varification </div><div className='icon'> <BsFillPatchCheckFill fontSize="1.3em" /></div> </div>
+            <div  id="btn-1" className='pointer text-white lg:text-sm font-medium 2xl:py-2 lg:py-2 lg:w-48 lg:px-2 w-32 py-2 px-1 text-xs 2xl:w-60 mt-2  rounded-lg  2xl:px-2 flex justify-between'> <div onClick={()=>{setshowModal(true) }}> Candidate varification </div><div className='icon'> <BsFillPatchCheckFill fontSize="1.3em" /></div> </div>
 {/* Proctor Group Chat */}
-            <div  id="btn-2" className=' hover:bg-blue-300 text-white lg:text-sm font-medium 2xl:py-2 lg:py-2 2xl:w-60 lg:w-44 rounded-lg w-36 py-2 px-1 mt-2 text-xs  2xl:px-6 lg:px-2 flex justify-between'><div> Proctor Group Chat</div> <div className='icon' > <IoMdChatboxes fontSize="1.5em" /></div></div>
+            <div  id="btn-2" className=' hover:bg-blue-300 text-white lg:text-sm font-medium 2xl:py-2 lg:py-2 2xl:w-60 lg:w-44 rounded-lg w-32 py-2 px-1 mt-2 text-xs  2xl:px-6 lg:px-2 flex justify-between'><div> Proctor Group Chat</div> <div className='icon' > <IoMdChatboxes fontSize="1.5em" /></div></div>
 {/* Status */}
 <div className='flex  pt-4 lg:pt-0' >
             <div id="btn-3div" className='2xl:text-sm displayclass py-2 lg:text-xs text-xs font-medium px-4 md:px-2'><b>Status</b>: <span id="btn-3" style={{backgroundColor:'#54FFB4'}}  className=' 2xl:text-sm text-xs font-medium py-1 rounded-xl px-3 '> IN PROGRESS</span></div>
