8ddccb01523d054bd16f6ca441582251ce71b99f
Muhammad Shah Sawar <50781826+shahsawar92@users.noreply.github.com>
2022-01-03 12:01:09 +0500

    resolved bar issue

--- src/Components/Home/Components/body.js
+++ src/Components/Home/Components/body.js
@@ -5,11 +5,11 @@ import Card2 from './newcard'
 export default function Body() {
     
     return (
-        <div className=' bg-white h-2/3 m-2  rounded-lg px-5'>
+        <div className=' bg-white h-2/3 m-2 pt-4  rounded-lg px-5'>
            
-            <div >
-                <div className='inline-block lg:pt-4 lg:pl-10 pt-2 lg:text-xl font-medium'>Exame Room</div>
-                <div className='w-5/6 float-right lg:pt-8 -mt-3'><hr /> </div>
+            <div className='flex flex-row  justify-between' >
+                <div className=' '>Exame Room</div>
+                <div className=' w-9/12 md:w-10/12 mt-3.5 '><hr /> </div>
             </div>
             <div className='flex justify-start' >
                <Card2 />  
